edc1216c2b5048efd6e8b06d928d462cc801c77e
Yasikaa-007 <thiagarajanyasikaa@gmail.com>
2026-02-21 17:58:02 +0530

    Create ViewPatientServlet.java

---
+++ ViewPatientServlet.java
@@ -0,0 +1,17 @@
+package com.hospital.servlet; 
+import java.io.IOException; 
+import java.util.List; 
+import javax.servlet.*; 
+import javax.servlet.http.*; 
+import com.hospital.dao.*; 
+import com.hospital.model.Patient; 
+public class ViewPatientServlet extends HttpServlet { 
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) 
+            throws ServletException, IOException { 
+        PatientDAO dao = new PatientDAOImpl(); 
+        List<Patient> list = dao.getAllPatients(); 
+        request.setAttribute("list", list); 
+        RequestDispatcher rd = request.getRequestDispatcher("view/view_patients.jsp"); 
+        rd.forward(request, response); 
+    } 
+} 
